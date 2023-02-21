9c6b3ae58ffe3ebdc293ba41a8b2356cb4a24142
Abdullah-Iftikhar <abdullah.laraveldev@gmail.com>
2023-02-21 23:47:59 +0500

    custom-permission

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->foreignId('role_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
